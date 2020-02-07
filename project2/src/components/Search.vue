205622f007f33b7acd6f8ff9c3d1984443e0abf9
Ryan James O'Laughlin <ryanjo9@blackbird.cs.byu.edu>
2020-02-06 19:26:58 -0700

    Added navbar

---
+++ project2/src/components/Search.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="search">
+    <h1>Add a book</h1>
+    <form @submit.prevent="login" class="pure-form pure-form-aligned">
+      <fieldset>
+      <div class="pure-control-group">
+        <!-- <label for="isbn">Username</label> -->
+        <input v-model="isbn" type="text" placeholder="Enter the ISB to autofill book info">
+      </div>
+
+      <div class="pure-controls">
+        <button type="submit" class="pure-button pure-button-primary">Next</button>
+        <button type="submit" class="pure-button pure-button-primary">Cancel</button>
+      </div>
+      </fieldset>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'search',
+  data() {
+    return {
+      isbn: ''
+    }
+  },
+  methods: {
+    async search() {
+      console.log(`Searching for isbn: ${this.isbn}`);
+    }
+  }
+}
+</script>
+
+<style scoped>
+form {
+  border: 1px solid #ccc;
+  background-color: #eee;
+  border-radius: 4px;
+  padding: 20px;
+}
+.pure-controls {
+  display: flex;
+}
+.pure-controls button {
+  margin-left: auto;
+}
+</style>
---
+++ project2/src/views/Add.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="add">
+    <Search />
+  </div>
+</template>
+
+<script>
+import Search from '@/components/Search.vue'
+
+export default {
+  name: 'Add',
+  components: {
+    Search
+  }
+}
+</script>
