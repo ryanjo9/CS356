6ad1ce7359449716cc3be82d9cd640f2caa85d12
camyngo <camyngo@nocsi.org>
2020-02-11 18:12:17 -0700

    remove linking on navbar

--- project2/src/App.vue
+++ project2/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <h1 class="navbar-brand" >Navbar</h1>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
