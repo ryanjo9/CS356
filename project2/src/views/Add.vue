f0c6e8ae050f44cefb3d6e8e444b7044d7bd737c
camyngo <camyngo@nocsi.org>
2020-02-11 17:27:52 -0700

    updates

--- project2/src/views/Add.vue
+++ project2/src/views/Add.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="add">
+  <div class="add bg-text">
     <Search />
   </div>
 </template>
@@ -16,5 +16,28 @@ export default {
 </script>
 
 <style scoped>
+body {
+  /* The image used */
+  background-color: black;
+  /* Add the blur effect */
+  filter: blur(8px);
+  -webkit-filter: blur(8px);
+}
 
+/* Position text in the middle of the page/image */
+.bg-text {
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0, 0.4); /* Black w/opacity/see-through */
+  color: white;
+  font-weight: bold;
+  border: 3px solid #f1f1f1;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 2;
+  width: 80%;
+  padding: 20px;
+  text-align: center;
+}
 </style>
