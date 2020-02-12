fc1c30d358dd8445424a32acbb2a0ee68623a6c8
camyngo <camyngo@nocsi.org>
2020-02-12 11:16:49 -0700

    LOL it will work now

--- project2/src/App.vue
+++ project2/src/App.vue
@@ -25,79 +25,7 @@
           </form>
         </div>
       </nav>
-      <br>
-      <br>
-      <br>
-
-      <br><br>
-      <br>
-
-      <br>
-      <br>
-      <br>
-
-      <br>
-      <br>
-      <br>
-      <br>
-
-      <br><br>
-      <br>
-
-      <br>
-      <br>
-      <br>
-
-      <br>
-      <br>
-      <br>
-      <br>
-
-      <br><br>
-      <br>
-
-      <br>
-      <br>
-      <br>
-
-      <br>
-      <br>
-      <br>
-      <br>
-
-      <br><br>
-      <br>
-
-      <br>
-      <br>
-      <br>
-
-      <br>
-      <br>
-      <br>
-      <br>
-
-      <br><br>
-      <br>
-
-      <br>
-      <br>
-      <br>
-
-      <br>
-      <br>
-      <br>
-      <br>
-
-      <br><br>
-      <br>
-
-      <br>
-      <br>
-      <br>
-
-      <br>
-
+      <router-view/>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -120,7 +48,6 @@
           </div>
         </nav>
     </div>
-    <router-view/>
   </div>
 </template>
 
