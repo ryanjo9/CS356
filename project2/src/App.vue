0874f601ea7b1c9abdde657784ad0f6445727777
camyngo <camyngo@nocsi.org>
2020-02-13 09:42:00 -0700

    merging conflct fix

--- project2/src/App.vue
+++ project2/src/App.vue
@@ -26,12 +26,6 @@
         </div>
       </nav>
       <router-view/>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-
-      <router-view/>
-
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
