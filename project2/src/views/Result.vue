eb2925f30ba1465a18b555ff3afc6c31128710c8
camyngo <camyngo@nocsi.org>
2020-02-17 23:09:38 -0700

    updates

--- project2/src/views/Result.vue
+++ project2/src/views/Result.vue
@@ -4,12 +4,12 @@
     
     <ul id="example-2">
     <span v-for="(book, position) in books" v-bind:key="position">
-        <div class="polaroid">
-        <img :src="book.imagePath"/>
+        <section class="box">
+        <img :src="book.imagePath" class="picture"/>
         {{ book.title }} - {{book.author}}
         Condition: {{ book.condition }}
         Price: {{ book.price }}
-        </div>
+        </section>
     </span>
     </ul>
   </div>
@@ -41,6 +41,11 @@ export default {
   }
 }
 </script>
-<style>
-
+<style scoped>
+.picture{
+  width:150px;
+}
+.box{
+  display: flex;
+}
 </style>
