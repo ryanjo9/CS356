35ffb45177142d06a26b2fdc6ca5fd59b08a2958
camyngo <camyngo@nocsi.org>
2020-02-29 17:06:15 -0700

    update logic for register

--- project2/src/views/Register.vue
+++ project2/src/views/Register.vue
@@ -5,20 +5,20 @@
     <div class="row">
         <div class="col" />
         <div class="col-6">
-        <form>
+        <form @submit.prevent="login">
             <p class="h4 text-center mb-4">Sign up</p>
             <div class="grey-text">
                 <mdbIcon icon="user" size="lg" class="icon"/>
-                <mdb-input required class="input" placeholder="Your name" type="text"/> <br/>
+                <mdb-input required class="input" v-model="username" laceholder="Your name" type="text"/> <br/>
                 <mdbIcon icon="envelope" size="lg" class="icon"/>
-                <mdb-input required class="input" placeholder="Your email" type="email"/> <br/>
+                <mdb-input required class="input" v-model="email" placeholder="Your email" type="email"/> <br/>
                 <mdbIcon icon="lock" size="lg" class="icon"/>
-                <mdb-input required class="input" placeholder="Your password" type="password"/> <br/>
+                <mdb-input required class="input" v-model="password" placeholder="Your password" type="password"/> <br/>
                 <mdbIcon icon="exclamation-triangle" size="lg" class="icon"/>
-                <mdb-input required class="input" placeholder="Confirm your password" type="text"/> <br/>
+                <mdb-input required class="input" v-model="recheck" placeholder="Confirm your password" type="text"/> <br/>
             </div>
             <div class="text-center">
-            <mdb-btn color="primary">Register</mdb-btn>
+            <mdb-btn color="primary" v-if="check">Register</mdb-btn>
             </div>
         </form>
         </div>
@@ -42,15 +42,23 @@ export default {
     return {
       username: '',
       password: '',
+      email: '',
+      recheck:'',
       error: '',
     }
+  },
+  computed:{
+      check(){
+         return this.password === this.recheck ? true : false
+      }
   },
   methods: {
     async login() {
       try {
         this.error = await this.$store.dispatch("login", {
           username: this.username,
-          password: this.password
+          password: this.password,
+            email: this.email
         });
         if (this.error === "")
           this.$router.push('/');
